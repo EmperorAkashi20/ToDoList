079677ea1cd946f0d5e87fc2eb0e02cd1565400b
Rishabh Sethia <rishabh@Rishabhs-MacBook-Air.local>
2021-09-14 18:06:36 +0530

    Structure changes

--- lib/ToDoList/Components/Body.dart
+++ lib/ToDoList/Components/Body.dart
@@ -1,13 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:loading_animations/loading_animations.dart';
-import 'package:todo/AddTask/Components/Body.dart';
+import 'package:todo/AddTask/AddTask.dart';
 import 'package:todo/Login.dart';
 import 'package:todo/main.dart';
 
